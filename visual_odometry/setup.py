479c85a7d5caaf77820b6adffff7d97de13e49c0
al-li-son <allisonli18710@gmail.com>
2022-10-25 14:42:35 -0400

    Create custom bringup.py launch file

--- visual_odometry/setup.py
+++ visual_odometry/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/bringup.py']),
+        ('share/' + package_name, ['Calibration Data/ost.yaml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
