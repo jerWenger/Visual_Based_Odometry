9b9b76cbbc09afb5ff0c0eccbbd797e785b6004a
al-li-son <allisonli18710@gmail.com>
2022-10-30 16:30:49 -0400

    Add functionality for tracking relative movement based on april tag position

--- visual_odometry/visual_odometry/helper_functions.py
+++ visual_odometry/visual_odometry/helper_functions.py
@@ -44,7 +44,26 @@ class TFHelper(object):
                                            z=rotation[2],
                                            w=rotation[3]))
 
-    
+    def get_tag_transform(self, camera_frame, tag_frame, timestamp):
+        if self.tf_buffer.can_transform(camera_frame, tag_frame, timestamp):
+            transform = self.tf_buffer.lookup_transform(camera_frame, tag_frame, timestamp)
+            tag_pose_frame = PyKDL.Frame(V=PyKDL.Vector(x=transform.transform.translation.x,
+                                                y=transform.transform.translation.y,
+                                                z=transform.transform.translation.z),
+                                R=PyKDL.Rotation.Quaternion(x=transform.transform.rotation.x,
+                                                            y=transform.transform.rotation.y,
+                                                            z=transform.transform.rotation.z,
+                                                            w=transform.transform.rotation.w))
+            tag_rotation_vector = (tag_pose_frame.M[0,2], tag_pose_frame.M[2,2])
+            tag_rotation_angle = np.arctan2(tag_rotation_vector[1], tag_rotation_vector[0])
+            # print(f"angle: {tag_rotation_angle * 180/np.pi}")
+            tag_position = (transform.transform.translation.x, transform.transform.translation.z)
+            # print(f"{tag_position=}")
+            tag_2d_transform = np.matrix([[np.cos(tag_rotation_angle), -np.sin(tag_rotation_angle), tag_position[0]],
+                                [np.sin(tag_rotation_angle), np.cos(tag_rotation_angle), tag_position[1]],
+                                [0, 0, 1]])
+            return tag_2d_transform
+        return None
 
     def get_matching_odom_pose(self, odom_frame, base_frame, timestamp):
         """ Find the odometry position for a given timestamp.  We want to avoid blocking, so if the transform is
